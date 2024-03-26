bc5fab6e8c03ed13981b5fc1eb8a43c9bd7d47bf
Kabeer Shivani <kabs225@hotmail.com>
2024-03-27 00:28:46 +0530

    refactored

---
+++ app/src/main/java/com/example/venditbuiz/machine_name1.java
@@ -0,0 +1,14 @@
+package com.example.venditbuiz;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class machine_name1 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_machine_name1);
+    }
+}
---
+++ app/src/main/java/com/example/venditbuiz/product_details1.java
@@ -0,0 +1,14 @@
+package com.example.venditbuiz;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class product_details1 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_product_details1);
+    }
+}
